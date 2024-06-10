edc8c72a7b9e689b46100bfd559b01bb3a5fa96c
Ashish2271 <ashispandey138c@gmail.com>
2024-06-10 17:12:19 +0530

    fix

--- apps/user-app/app/api/videos/test/route.ts
+++ apps/user-app/app/api/videos/test/route.ts
@@ -1,38 +1,38 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { createYoutubePost } from "../../../../actions/youtube";
-import { NextResponse } from "next/server";
-import * as cron from "node-cron";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { createYoutubePost } from "../../../../actions/youtube";
+// import { NextResponse } from "next/server";
+// import * as cron from "node-cron";
 
-let videos: any;
+// let videos: any;
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    // Call the createYoutubePost function here
-    videos = await createYoutubePost();
+// const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     // Call the createYoutubePost function here
+//     videos = await createYoutubePost();
 
-    NextResponse.json({
-      videos,
-      message: "Successfully fetched and processed Youtube posts.",
-    });
-  } catch (error: any) {
-    console.error("Error while processing Youtube posts:", error);
-    NextResponse.json({
-      message: "An error occurred while processing Youtube posts.",
-      error: error.toString(),
-    });
-  }
-};
+//     NextResponse.json({
+//       videos,
+//       message: "Successfully fetched and processed Youtube posts.",
+//     });
+//   } catch (error: any) {
+//     console.error("Error while processing Youtube posts:", error);
+//     NextResponse.json({
+//       message: "An error occurred while processing Youtube posts.",
+//       error: error.toString(),
+//     });
+//   }
+// };
 
-export const GET = async () => {
-  const schedule = process.env.CRON_SCHEDULE || "59 23 * * *";
-  cron.schedule(schedule, async () => {
-    console.info(`Running scheduled task "${schedule}"`);
-    await handler(null!, null!);
-  });
+// export const GET = async () => {
+//   const schedule = process.env.CRON_SCHEDULE || "59 23 * * *";
+//   cron.schedule(schedule, async () => {
+//     console.info(`Running scheduled task "${schedule}"`);
+//     await handler(null!, null!);
+//   });
 
    
-  return NextResponse.json({
-    message: "Starting scheduled task",
-    videos,
-  });
-};
+//   return NextResponse.json({
+//     message: "Starting scheduled task",
+//     videos,
+//   });
+// };
